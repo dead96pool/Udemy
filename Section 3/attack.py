907d303d6967d45f9a20b75aca967e1b034b9a28
dead96pool <sachleensingh975@gmail.com>
2023-04-14 00:05:13 +0530

    update

--- Section 3/attack.py
+++ Section 3/attack.py
@@ -1,5 +1,7 @@
 import random
 
+
+'''
 playerhp = 260
 enemyattklow = 60     # enemy attack low value 
 enemyattkhi = 80      # enemy attack high value
@@ -8,13 +10,16 @@ while playerhp > 0:
   dmg = random.randrange(enemyattklow, enemyattkhi)
   playerhp -= dmg
   
-  if playerhp < 30:
+  if playerhp < 30:     # player health doesnt go below 30
     playerhp = 30
   
   print("Attack for ",dmg," points. Remaining HP is ",playerhp)
     
   if playerhp > 30:
-    continue
+    continue            # continue to while if hp above 30 
   
+  # when player hp is at 30
   print("HP too low. You have been teleported to the nearest inn.")
   break
+
+'''
