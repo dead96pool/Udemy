e329e1667795d552d98d75a8dc1baa0492b1417f
dead96pool <49323402+dead96pool@users.noreply.github.com>
2023-04-13 23:32:41 +0530

    update Section 3/attack.py

--- Section 3/attack.py
+++ Section 3/attack.py
@@ -1,6 +1,15 @@
+import random
 
 playerhp = 260
-enemyattklow = 60 
-enemyattkhi = 80
+enemyattklow = 60     # enemy attack low value 
+enemyattkhi = 80      # enemy attack high value
 
 
+while playerhp > 0:
+  dmg = random.randrange(enemyattklow, enemyattkhi)
+  playerhp -= dmg
+  
+  if playerhp <= 0:
+    playerhp = 0
+  
+  print("Attack for ",dmg," points. Remaining HP is ",playerhp)
