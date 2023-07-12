dbfa584a2a4257a9da1ca8979a4761720420f438
dead96pool <sachleensingh975@gmail.com>
2023-07-12 22:24:07 +0530

    update

--- Section 4/project 2/battle/classes/game.py
+++ Section 4/project 2/battle/classes/game.py
@@ -11,17 +11,19 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 class Person:
+    # initializing the variables
     def __init__(self, hp, mp, atk, df, magic):
-        self.maxhp = hp
-        self.hp = hp
-        self.maxmp = mp
-        self.mp = mp
-        self.atkl = atk - 10
-        self.atkh = atk + 10
+        self.maxhp = hp                                 # max HP
+        self.hp = hp                                    # current hp
+        self.maxmp = mp                                 # max MP
+        self.mp = mp                                    # current MP
+        self.atkl = atk - 10                            # low atk
+        self.atkh = atk + 10                            # high atk
         self.df = df
         self.magic = magic
         self.actions = ["Attack", "Magic"]
     
+    # method to generate random damage points
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
     
