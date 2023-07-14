c17a99a1bb1c367ec49332b981cc22a5471040ff
dead96pool <sachleensingh975@gmail.com>
2023-07-14 18:34:11 +0530

    magic damage

--- Section 4/project 2/battle/classes/game.py
+++ Section 4/project 2/battle/classes/game.py
@@ -1,4 +1,5 @@
 import random
+#from classes.magic import Spell
 
 class bcolors:
     HEADER = '\033[95m'
@@ -27,17 +28,17 @@ class Person:
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
 
-    def generate_spell_damage(self, i):
-        mgl = self.magic[i]["dmg"] - 5
-        mgh = self.magic[i]["dmg"] + 5
-        return random.randrange(mgl, mgh)
-    
     def take_damage(self, dmg):
         self.hp -= dmg
         if self.hp < 0:
             self.hp = 0
         return self.hp
     
+    def heal(self, dmg):
+        self.hp += dmg
+        if self.hp > self.maxhp:
+            self.hp = self.maxhp
+
     def get_hp(self):
         return self.hp
     
@@ -53,23 +54,16 @@ class Person:
     def reduce_mp(self, cost):
         self.mp -= cost
 
-    def get_spell_name(self, i):
-        return self.magic[i]["name"]
-    
-    def get_spell_mp_cost(self, i):
-        return self.magic[i]["cost"]
-    
-
     def choose_action(self):
         i = 1
-        print("Actions")
+        print(bcolors.OKBLUE + bcolors.BOLD + "Actions" + bcolors.ENDC)
         for item in self.actions:
             print(str(i) + ":", item)
             i += 1
 
     def choose_magic(self):
         i = 1
-        print("Magic")
+        print(bcolors.OKBLUE + bcolors.BOLD + "Magic" + bcolors.ENDC)
         for spell in self.magic:
-            print(str(i) + ":", spell["name"], "(Cost:", str(spell["cost"]), ")")
+            print(str(i) + ":", spell.name, "(Cost:", str(spell.cost)+")")
             i += 1
--- Section 4/project 2/battle/main.py
+++ Section 4/project 2/battle/main.py
@@ -1,12 +1,22 @@
 from classes.game import Person, bcolors
+from classes.magic import Spell
 
-magic = [{"name": "Fire",       "cost": 10,     "dmg": 60},
-         {"name": "Thunder",    "cost": 12,     "dmg": 80},
-         {"name": "Blizzard",   "cost": 10,     "dmg": 60}]
+# instantiate Black Magic
+fire = Spell("Fire", 10, 100, "black")
+thunder = Spell("Thunder", 10, 100, "black")
+blizzard = Spell("Blizzard", 10, 100, "black")
+meteor = Spell("Meteor", 20, 200, "black")
+quake = Spell("Quake", 14, 140, "black")
 
+# create White Magic
+cure = Spell("Cure",  12, 120, "white")
+cura = Spell("Cura", 18, 200, "white")
+
+
+# Intantiate People
 # person class (hp, mp, atk, df, magic) argumetns in order
-player = Person(460, 65, 60, 34, magic)
-enemy = Person(1200, 65, 45, 25, magic)
+player = Person(460, 65, 60, 34, [fire, thunder, blizzard, meteor, cure, cura])
+enemy = Person(1200, 65, 45, 25, [])
 
 running = True
 i = 0
@@ -17,4 +27,53 @@ while running:
     print("=============================")
     player.choose_action()
 
-    running = False
+    choice = input("Enter choice: ")
+    index = int(choice) - 1
+    
+    # player choice between attack or magic
+    # attack
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print("You attacked for", dmg, "points of damage.")
+
+    # player chose magic
+    elif index == 1:
+        player.choose_magic()
+        magic_choice = int(input("Choose magic: ")) - 1                     # to account for the first index in list starting from 0 not 1
+
+        spell = player.magic[magic_choice]                  # going to the magic class object though the People class object | person's magic's object
+        magic_dmg = spell.generate_damage()
+        
+        
+        current_mp = player.get_mp()
+
+        if spell.cost > current_mp:
+            print(bcolors.FAIL + "\nNot enough MP\n" + bcolors.ENDC)
+            continue
+
+        player.reduce_mp(spell.cost)
+        enemy.take_damage(magic_dmg)
+        print(bcolors.OKBLUE + "\n" + spell.name + " deals", magic_dmg, "points of damage." + bcolors.ENDC)
+
+    #enemy turn to attack or magic
+    enemy_choice = 1                    # enemy chose to attack
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print("Enemy attacked for", enemy_dmg, "points of damage.")
+
+    # end of player and enemy turn
+    # display both player and enemy HP/MAX_HP
+    print("=============================")
+    print("Enemy HP: ", bcolors.FAIL + str(enemy.get_hp()) + "/" + str(enemy.get_max_hp()) + bcolors.ENDC + "\n")
+    
+    print("Your HP: " + bcolors.OKGREEN + str(player.get_hp()) + "/" + str(player.get_max_hp()) + bcolors.ENDC)
+    print("Your MP: " + bcolors.OKBLUE + str(player.get_mp()) + "/" + str(player.get_max_mp()) + bcolors.ENDC + "\n")
+    
+    # end look if hp reaches 0
+    if enemy.get_hp() == 0:
+        print(bcolors.OKGREEN + "You win!" + bcolors.ENDC)
+        running = False
+    elif player.get_hp() == 0:
+        print(bcolors.FAIL + "Your enemy has defeated you!" + bcolors.ENDC)
+        running = False
